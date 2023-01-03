42bf8c9f8970c9e718b4836659c15856a4cb5d5e
Omkar Dnyaneshwar Udhane <omkarudhane10@gmail.com>
2023-01-03 22:09:24 +0530

    curd with mongoose

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const dbConnect = require('./mongodb');
+const express = require('express');
+require('./config');
+const User = require('./user');
 
-const main = async () => {
-  let data = await dbConnect();
-  data = await data.find().toArray();
-};
-main();
+const app = express();
+app.use(express.json());
+
+app.post('/create', async (req, resp) => {
+  let data = new User(req.body);
+  let result = await data.save();
+  console.log(result);
+  resp.send(result);
+});
+app.get('/list', async (req, resp) => {
+  let result = await User.find();
+  console.log(result);
+  resp.send(result);
+});
+app.put('/update/:_id', async (req, resp) => {
+  let result = await User.updateOne(req.params, { $set: req.body });
+  console.log(result);
+  resp.send(result);
+});
+app.delete('/delete/:_id', async (req, resp) => {
+  let result = await User.deleteOne(req.params);
+  console.log(result);
+  resp.send(result);
+});
+
+app.listen(8000);
